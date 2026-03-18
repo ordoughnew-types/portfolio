9624200607fe45ae50d2393a3000e2b6ba2c73b1
Adrian James Ordonio <adrianordonioworks@gmail.com>
2026-03-19 04:12:07 +0800

    Trying again vercel

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
       <h1 className="text-4xl font-bold">
-        Hi, I'm Elle 👋
+        Helloow, Good Morning bb! 👋
       </h1>
     </main>
   );
