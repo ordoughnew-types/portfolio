6ea8e62da824116bdf3d032f16fd0050366d78c4
Adrian James Ordonio <adrianordonioworks@gmail.com>
2026-03-23 04:00:27 +0800

    Changing function name Home to MainPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function MainPage() {
   return (
     <main className="flex min-h-screen items-center justify-center">
       <h1 className="text-4xl font-bold">
