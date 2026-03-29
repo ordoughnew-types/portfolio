e04707ad8580066a94e3215a854445d3e713c547
Adrian James Ordonio <adrianordonioworks@gmail.com>
2026-03-30 01:55:55 +0800

    Added font and resized images

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
     <section className="w-full min-h-screen flex justify-center items-start">
 
       {/* Outer rectangle container */}
-      <div className="w-full max-w-7xl p-10 rounded-lg shadow-lg flex flex-col gap-8 bg-[#f5f5dc] relative min-h-[500px] overflow-hidden">
+      <div className="w-full max-w-10xl p-10 rounded-lg shadow-lg flex flex-col gap-8 bg-[#f5f5dc] relative min-h-[1000px] overflow-hidden">
 
         {/* Name on header */}
         <div>
@@ -21,32 +21,26 @@ export default function Hero() {
           <p className="text-lg">
             Full Stack | AI | Data Science
           </p>
+        </div> 
+
+        {/* BGDesign */}
+        <div className="absolute top-0 right-0 w-[500px] h-[950px] flex flex-col md:flex-row gap-6">
+          <Image
+              src="/BGDesign-L.png"
+              alt="Just a Design"
+              fill
+              className="object-cover rounded-md shadow-md"
+          />
         </div>
 
-        {/* BGDesign and ProfilePic container */}
-        <div className="flex flex-col md:flex-row gap-6">   
-
-          {/* BGDesign */}
-          <div className="absolute top-0 right-0">
-            <Image
-                src="/BGDesign-L.png"
-                alt="Adrian Ordonio"
-                width={420}
-                height={100} // shorter than original
-                className="rounded-md shadow-md"
-            />
-          </div>
-
-          {/* ProfilePic */}
-          <div className="absolute top-20 right-75">
-            <Image
-                src="/InformalPic.JPG"
-                alt="Adrian Ordonio"
-                width={300}  // set width in pixels
-                height={400} // set height in pixels
-                className="rounded-md shadow-md"
-            />
-          </div>
+        {/* ProfilePic */}
+        <div className="absolute top-40 right-40 w-[440px] h-[590px] flex flex-col md:flex-row gap-6">
+          <Image
+              src="/InformalPic.JPG"
+              alt="Adrian Ordonio"
+              fill
+              className="rounded-md shadow-md"
+          />
         </div>
       </div>
     </section>
